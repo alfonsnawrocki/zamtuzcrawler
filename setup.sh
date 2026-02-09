d55311f58c2ee351981826bd9443d816132a1d4e
alfonsnawrocki <emoji.alerty6@icloud.com>
2026-02-09 09:28:44 +0100

    Enhance setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@ IFS=$'\n\t'
 npm install webtorrent-cli -g
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv tool install bt1337xearch
+
+mkdir downloads
+cd downloads/
+
+# search: 
+echo "bt1337xearch -n "nicollubin" 
+echo  "webtorrent download $(MAGNETLNK)"  
+
+# to share: 
+go run github.com/eliben/static-server@latest
